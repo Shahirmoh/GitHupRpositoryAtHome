4bad147dce64d1c766305eb038e921986aaf483a
Shahirmoh <ghawsshahir8@gmail.com>
2021-07-30 16:06:44 -0700

    this is my test message

---
+++ src/main/java/GitHupPracticeAtHome/GitHupPracticeAtHome/App.java
@@ -0,0 +1,13 @@
+package GitHupPracticeAtHome.GitHupPracticeAtHome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
