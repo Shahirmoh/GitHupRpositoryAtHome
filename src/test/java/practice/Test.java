94463fd313895fef158eb0858d131ce29aec55ce
Shahirmoh <88009558+Shahirmoh@users.noreply.github.com>
2021-07-30 16:20:10 -0700

    Update Test.java
    I did changes.

--- src/test/java/practice/Test.java
+++ src/test/java/practice/Test.java
@@ -3,7 +3,9 @@ package practice;
 public class Test {
 	
 	public void testGitHub() {
-		System.out.println("this is my GitHupmetheod");
+		System.out.println("this is my GitHupmetheod")
+		// I make this changes in github
+			Systen.out.printlin(I make this changes in githu);
 	}
 
 }
